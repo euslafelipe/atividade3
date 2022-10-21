8a085b1ace364fbe67f00cd52492e9c3535996e7
euslafelipe <felipeferraz2332@gmail.com>
2022-10-21 20:36:51 -0300

    Questao03

---
+++ Questao03.java
@@ -0,0 +1,40 @@
+package atividade3;
+
+import java.util.Scanner;
+
+public class Questao03 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Introduza um numero :");
+        int num1 = ler.nextInt();
+        System.out.println("Introduza outro numero :");
+        int num2 = ler.nextInt();
+
+        System.out.println("os numeros entre "+num1+" e "+num2+" é:");
+
+        if (num1>num2) {
+            for (int i =num1; i>=num2; i=i-1){
+                System.out.println(i);
+    
+                
+            }
+
+            
+        }else{
+            for (int i =num1; i<=num2; i=i+1){
+
+                System.out.println(i);
+    
+                
+            }
+        
+
+        }
+            
+        
+        
+    ler.close();
+    }
+}
