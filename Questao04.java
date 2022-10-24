2e1e708ee344ced274f6579816b5ff788c604681
euslafelipe <felipeferraz2332@gmail.com>
2022-10-24 20:41:10 -0300

    Questoes

---
+++ Questao04.java
@@ -0,0 +1,27 @@
+package atividade3;
+
+
+import java.util.Scanner;
+
+public class Questao04 {
+    
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+                int chances = 5;
+               do{
+                System.out.println("Digite sua senha: ");
+                String senha = new String(ler.nextLine());
+                chances = chances - 1;
+                if(senha.equals("Java-2022")){
+                    System.out.println("Palavra ok");
+                }else
+                    System.out.println("Palavra Errada");
+                }while(chances >0);
+                System.out.println("excedeu o limite de tentativas");
+        ler.close();
+     }
+  }    
+        
+
+    
+
---
+++ Questao05.java
@@ -0,0 +1,35 @@
+package atividade3;
+
+
+import java.util.Scanner;
+
+public class Questao05 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int pessoas = 0;
+        int pessoas1 = 0;
+
+        for(int i =1;i <= 5;i=i+1){
+            System.out.print("Digite a idade: ");
+            int idade = ler.nextInt();
+            
+           
+            if(idade >= 18){
+            pessoas++;
+
+        }else{
+            pessoas1++;
+
+        }
+        
+        }
+        System.out.println("A quantidade de pessoas com idade maior ou igual a 18 anos é de " + pessoas +" e pessoas menores de 18 anos é de: "+pessoas1);
+
+    ler.close();
+    }
+
+}
+    
+
+
